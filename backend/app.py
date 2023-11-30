5cd9632befd61db576b1d8b66374c1745b6bc5fa
Robin_Dillen <Robin.Dillen@student.uantwerpen.be>
2023-11-30 17:40:54 +0100

    use model

--- backend/app.py
+++ backend/app.py
@@ -4,16 +4,36 @@ from flask import Flask, render_template, request, jsonify
 import praw
 import os
 import dotenv
-from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
+import torch
+import pandas as pd
+import numpy as np
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer
 
 
 dotenv.load_dotenv()
 
+
 app = Flask(__name__)
 
-# Init. PRAW
 reddit = praw.Reddit(client_id=os.getenv('CLIENT_ID'), client_secret=os.getenv('CLIENT_SECRET'),
                      user_agent=os.getenv('USER_AGENT'), username=os.getenv('USERNAME'), password=os.getenv('PASSWORD'))
+
+# load tokenizer and model, create trainer
+model_name = "j-hartmann/emotion-english-distilroberta-base"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+trainer = Trainer(model=model)
+
+# Create class for data preparation
+class Posts:
+    def __init__(self, tokenized_texts):
+        self.tokenized_texts = tokenized_texts
+
+    def __len__(self):
+        return len(self.tokenized_texts["input_ids"])
+
+    def __getitem__(self, idx):
+        return {k: v[idx] for k, v in self.tokenized_texts.items()}
 
 @app.route('/')
 def index():
@@ -28,9 +48,6 @@ def index():
 @app.route('/back_posts')
 def get_posts():
     category = request.args.get('category', 'all')
-    tokenizer = AutoTokenizer.from_pretrained("j-hartmann/emotion-english-distilroberta-base")
-    model = AutoModelForSequenceClassification.from_pretrained("j-hartmann/emotion-english-distilroberta-base")
-    classifier = pipeline("text-classification", model=model, tokenizer=tokenizer, return_all_scores=True)
 
     posts = [
         {'title': 'First Post', 'content': 'This is the first post.'},
@@ -40,6 +57,46 @@ def get_posts():
         # More posts...
     ]
 
+    pred_texts = [post['content'] for post in posts]
+    # Tokenize texts and create prediction data set
+    tokenized_texts = tokenizer(pred_texts, truncation=True, padding=True)
+    pred_dataset = Posts(tokenized_texts)
+
+    # Run predictions
+    predictions = trainer.predict(pred_dataset)
+
+    # Transform predictions to labels
+    preds = predictions.predictions.argmax(-1)
+    labels = pd.Series(preds).map(model.config.id2label)
+    scores = (np.exp(predictions[0]) / np.exp(predictions[0]).sum(-1, keepdims=True)).max(1)
+
+    print((np.exp(predictions[0]) / np.exp(predictions[0]).sum(-1, keepdims=True)))
+
+    # scores raw
+    temp = (np.exp(predictions[0]) / np.exp(predictions[0]).sum(-1, keepdims=True))
+
+    # work in progress
+    # container
+    anger = []
+    disgust = []
+    fear = []
+    joy = []
+    neutral = []
+    sadness = []
+    surprise = []
+
+    # extract scores (as many entries as exist in pred_texts)
+    for i in range(len(pred_texts)):
+        anger.append(temp[i][0])
+        disgust.append(temp[i][1])
+        fear.append(temp[i][2])
+        joy.append(temp[i][3])
+        neutral.append(temp[i][4])
+        sadness.append(temp[i][5])
+        surprise.append(temp[i][6])
+
+    print(anger, disgust, fear, joy, neutral, sadness, surprise)
+
     for post in posts:
         print(post['content'])
         print(classifier(post['content'][0]))
@@ -48,4 +105,4 @@ def get_posts():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
